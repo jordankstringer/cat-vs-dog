cc16bdfcbeb5abf1c18567d7a620ecbcba7faeed
Carlos Urquiza and Jordan Stringer <webquiza@gmail.com>
2016-05-23 20:00:28 -0700

    This will add picture of cute kitten

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,17 @@ $(document).ready(function() {
     $("ul#dog").prepend("<li>WOOF!</li>");
     $("ul#cat").prepend("<li>Meow.</li>");
 
-    $('li').css('background-color', 'green');
   });
 
   $("button#meow").click(function() {
     $("ul#dog").prepend("<li>Meow</li>");
     $("ul#cat").prepend("<li>WOOF!</li>");
 
-    $('li').css('background-color', 'green');
+  });
+
+    $("p").click(function() {
+      $(".animal-showing").toggle();
+      $(".animal-hidden").toggle();
+
   });
 });
